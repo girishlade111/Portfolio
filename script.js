3a2ce9f9d21b886d60a76f10fa1cd5b8f6f2b704
Girish balaso Lade <girishlade111@gmail.com>
2025-07-16 16:35:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("particles.js");
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+var projectContainer = document.getElementById("project-container");
+fetch("https://api.github.com/users/girishlade111/repos")
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    data.forEach(function (repo) {
+        var projectCard = document.createElement("div");
+        projectCard.classList.add("project-card");
+        var projectName = document.createElement("h3");
+        projectName.innerText = repo.name;
+        var projectDescription = document.createElement("p");
+        projectDescription.innerText = repo.description;
+        var projectLink = document.createElement("a");
+        projectLink.href = repo.html_url;
+        projectLink.innerText = "View on GitHub";
+        projectCard.appendChild(projectName);
+        projectCard.appendChild(projectDescription);
+        projectCard.appendChild(projectLink);
+        projectContainer.appendChild(projectCard);
+    });
+});
